7fd85a7c6e553fce4e299372fe284eb6dff0d7de
Niklas Rosenkilde Rasmussen <niklas.rosenkilde@oulook.com>
2020-02-21 21:50:56 +0100

    Created project

---
+++ plastastic/ribbon/ribbon.cs
@@ -0,0 +1,108 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Windows.Media.Imaging;
+
+namespace Ribbon
+{
+
+    // Knap class. For hvert script/funktion oprettes en Knap med data om funktionen/scriptet.
+    public class Button
+    {
+        public string interntKnapnavn;
+        public string synligtKnapNavn;
+        public string namespaceNavn;
+        public string className;
+        public string kortTooltip;
+        public string langtTooltip;
+        public string panelNavn;
+        public string ikonFilnavn;
+
+        /// <summary>
+        /// Knap Constructor. Opretter Knap ved angivelse af tilhørende data
+        /// </summary>
+        /// <param name="interntKnapnavn"> Fremgår ikke i Revit </param>
+        /// <param name="synligtKnapNavn"     >Navn, som ses under selve knappen i Revit</param>
+        /// <param name="namespaceNavn">Navn på namespace i tilhørende dll-fil</param>
+        /// <param name="className"     >Navn på den class, som skal køres</param>
+        /// <param name="kortTooltip"  >Kort tooltip, der vises med det samme i Revit ved mouseover</param>
+        /// <param name="langtTooltip" >Langt tooltip, der folder sig ned efter nogle sekunders mouseover</param>
+        /// <param name="panelNavn"    >Navn på det panel, knappen hører under</param>
+        /// <param name="ikonFilnavn"  >// Knap-ikon (png eller ico). 32x32px for store ikoner, 16x16px for små ikoner</param>
+        public Button(string interntKnapnavn, string synligtKnapNavn, string namespaceNavn, string className, string kortTooltip, string langtTooltip, string panelNavn, string ikonFilnavn)
+        {
+
+            this.interntKnapnavn = interntKnapnavn; // Fremgår ikke i Revit
+            this.synligtKnapNavn = synligtKnapNavn; // Navn, som ses under selve knappen i Revit
+            this.namespaceNavn = namespaceNavn;   // Navn på namespace i tilhørende dll-fil
+            this.className = className;       // Navn på den class, som skal køres
+            this.kortTooltip = kortTooltip;     // Kort tooltip, der vises med det samme i Revit ved mouseover
+            this.langtTooltip = langtTooltip;    // Langt tooltip, der folder sig ned efter nogle sekunders mouseover
+            this.panelNavn = panelNavn;       // Navn på det panel, knappen hører under
+            this.ikonFilnavn = ikonFilnavn;     // Knap-ikon (png eller ico). 32x32px for store ikoner, 16x16px for små ikoner
+        }
+    }
+
+    class Ribbons : IExternalApplication
+    {
+        public Result OnStartup(UIControlledApplication application)
+        {
+            // Laver RibbonTab 
+            string ribbonName = "deswaks";
+            application.CreateRibbonTab(ribbonName);
+
+            string versionNumber = application.ControlledApplication.VersionNumber;
+
+            // Sti til denne dll (sti til dll med dét script, en knap skal køre):
+            string thisAssemblyPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+
+            ////////////////
+            //// PANELS ////
+            // Laver ribbon panel kaldt "Sheets" under fanen "Rubow"
+            Dictionary<string, RibbonPanel> buttonPanel = new Dictionary<string, RibbonPanel>();
+
+            // Navne på panels
+            List<string> panelNames = new List<string>(new string[]{
+                "STL Export"
+            });
+
+            // Opretter RibbonPanel for hvert navn:
+            foreach (string panelName in panelNames)
+            {
+                buttonPanel.Add(panelName, application.CreateRibbonPanel(ribbonName, panelName));
+            }
+
+            // //////////
+            // BUTTONS //
+            // Liste med knapper for alle scripts. Hver knap angiver et script
+            List<Button> buttons = new List<Button>
+            {
+                new Button("stlExport", "Export STL", "ExportSTL", "ExportSTL", "Exports an STL","", "STL Export", "exportstl.PNG"),
+
+            };
+
+            // Opretter knapper på Revit ribbon for hvert script i scriptOversigt
+            foreach (Button but in buttons)
+            {
+                Uri bt_ikon = new Uri(@"C:\ProgramData\Autodesk\Revit\Addins\" + versionNumber + @"\deswaks\" + but.ikonFilnavn);
+
+                PushButtonData bt = new PushButtonData(but.interntKnapnavn, but.synligtKnapNavn, thisAssemblyPath, but.namespaceNavn + "." + but.className)
+                {
+                    AvailabilityClassName = but.namespaceNavn + ".Availability",
+                    ToolTip = but.kortTooltip,
+                    LongDescription = but.langtTooltip,
+                    LargeImage = new BitmapImage(bt_ikon)
+                };
+                buttonPanel[but.panelNavn].AddItem(bt);
+            }
+            return Result.Succeeded;
+        }
+
+        // Lukker ribbon når Revit lukkes
+        public Result OnShutdown(UIControlledApplication application)
+        {
+            return Result.Succeeded;
+        }
+
+    }
+}
---
+++ plastastic/stlExport/ExportSTL.cs
@@ -0,0 +1,50 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+
+namespace ExportSTL
+{
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    public class ExportSTL : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document uidoc = commandData.Application.ActiveUIDocument.Document;
+            Document doc = uiDoc.Document;
+
+
+            //FilteredElementCollector finder alle sheets i dokumentet
+            List<Element> alleSheets = new FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).ToElements().ToList();
+
+
+            // Starter transaction
+            Transaction t = new Transaction(doc);
+            t.Start("Export STL");
+
+
+
+            // Transaction close:
+            t.Commit();
+            
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+
+    // The Availability Class must be added before the project can be loaded into Revit:
+    public class Availability : IExternalCommandAvailability
+    {
+        public bool IsCommandAvailable(UIApplication a, CategorySet b) { return true; }
+    }
+}
